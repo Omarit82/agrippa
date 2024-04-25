c1ef43574611932424e60e2e78b20525a9edb622
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-04-25 15:16:50 -0300

    feature/primeraVersion - Carpeta app y router avanzado

---
+++ config/ConfigApp.php
@@ -0,0 +1,13 @@
+<?php
+
+class ConfigApp{
+    public static $ACTION = 'action';
+    public static $PARAMS = 'params';
+    public static $ACTIONS = [
+        ''=>'TareasController#home',
+        'home'=>'TareasController#home',
+        'tareas'=>'TareasController#home',
+        'add'=>'TareasController#insertTarea',
+        'erase'=>'TareasController#eraseTarea'
+    ];
+}
---
+++ routerAdvanced.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config/ConfigApp.php';
+require_once 'app/controller/PacienteController.php';
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+define('ACTION',0);
+define('PARAMS',1);
+function parseURL($url){
+    $urlExploded = explode('/',$url);
+    $arrayReturn[ConfigApp::$ACTION] = $urlExploded[ACTION];
+    $arrayReturn[ConfigApp::$PARAMS] = isset($urlExploded[PARAMS]) ? array_slice($urlExploded,1) : null;
+    return $arrayReturn;
+}
+
+if(isset($_GET['action'])){
+    $urlData = parseURL($_GET['action']);
+    $action = $urlData[ConfigApp::$ACTION]; // home
+    if(array_key_exists($action,ConfigApp::$ACTIONS)){
+        $params = $urlData[ConfigApp::$PARAMS];
+        $action = explode('#',ConfigApp::$ACTIONS[$action]);
+        $controller = new $action[0]();
+        $metodo = $action[1];
+        if(isset($params) && $params != null){
+            echo $controller->$metodo($params);
+        }else{
+            echo $controller->$metodo();
+        }
+    }
+}
